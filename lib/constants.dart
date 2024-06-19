2715630db81a2f115b0894c432f5332a0076be02
Vijay Jha <jvdvijay@gmail.com>
2024-06-20 00:21:07 +0530

    Onboarding : Weight Screen

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xff511ccc);
+const primaryTextColor = Color.fromARGB(255, 255, 255, 255);
+const secondaryTextColor = Color.fromARGB(255, 0, 0, 0);
+const tertiaryTextColor = Color(0xffEBFF00);
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:fitness_app/screens/onboarding/onboarding_screen.dart';
 import 'package:fitness_app/size_config.dart';
 import 'package:flutter/material.dart';
-// import 'package:unbording/onbording.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +12,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
+        fontFamily: 'Poppins',
       ),
       home: OnbordingScreen(),
     );
